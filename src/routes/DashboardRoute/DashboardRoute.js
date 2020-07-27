f78e48eff76120cc30087fd66f2bf3685a3af419
M Jones <jonesman2800@gmail.com>
2020-07-27 17:37:00 -0400

    lite format for dashboard

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react'
 class DashboardRoute extends Component {
   render() {
     return (
-      <section>
-        implement and style me
-      </section>
+      <div>
+        <section>
+          <h2>American Sign Language</h2>
+          <button>Start Practice</button>
+        </section>
+        <section>
+          Total Score: __ Correct
+          <div className='Card'>
+            <div>image here</div>
+            0 Correct, 0 Incorrect
+          </div>
+        </section>
+      </div>
     );
   }
 }
