d9ec19be5560e255d73588e88f5514af0f88477e
M Jones <jonesman2800@gmail.com>
2020-08-03 21:58:38 -0400

    fixed config and vercel file for deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  API_ENDPOINT: 'https://clerc-asl.herokuapp.com/api',
+  TOKEN_KEY: 'E5B9FD28-3926-4614-B69A-45D4C9003AF4',
 }
